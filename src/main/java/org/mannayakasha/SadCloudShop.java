f272fe15de1261aca207c9c8782578eddbbd44a5
Pavel Milovidov <mpv9@tut.by>
2018-02-17 17:00:40 +0300

    jwt2

--- src/main/java/org/mannayakasha/SadCloudShop.java
+++ src/main/java/org/mannayakasha/SadCloudShop.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Class, that contains method main() and launches the app.
@@ -28,5 +31,10 @@ public class SadCloudShop extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SadCloudShop.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
